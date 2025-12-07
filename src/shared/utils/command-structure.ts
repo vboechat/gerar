45ca9f6d27c393e63f1ebefab98f3307aabc2697
Victor Ribeiro Boechat <victor2005contato@gmail.com>
2025-12-07 00:18:04 -0300

    refactor: remove command structure
    Command structure file will be replaced by command builder file in the next commit.

--- src/shared/utils/command-structure.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from "commander";
-
-export type CommandStructure = {
-  name: string;
-  description: string;
-  action: (this: Command, ...args: any[]) => void | Promise<void>;
-};
